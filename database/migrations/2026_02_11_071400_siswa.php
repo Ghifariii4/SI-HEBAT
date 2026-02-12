6144800ef3b491d673af5829213045f6eaeb0009
Ghifariii4 <ahmadghifari831@gmail.com>
2026-02-12 16:36:15 +0700

    Perbaikan psql

---
+++ database/migrations/2026_02_11_071400_siswa.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->id();
+            $table->varchar(18)('nama_siswa');
+            $table->varchar(100)('email')->unique();
+            $table->varchar(8)('password');
+            $table->t
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
